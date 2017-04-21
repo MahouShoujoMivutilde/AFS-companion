b60f2748339a5154986169ad21f9801c7a237415
MahouShoujoMivutilde <MahouShoujoMivutilde@users.noreply.github.com>
2017-04-22 00:33:38 +0300

    блид обоих (cli и фоновой) версий сразу в одном месте

--- setup.py
+++ setup.py
@@ -1,15 +1,12 @@
 import sys
 from cx_Freeze import setup, Executable
-from os import path
+from os import path, remove
+from shutil import copyfile
 
-base = None
-if sys.platform == "win32":
-    base = "Win32GUI"
-
-ver = "1.2.1"
+ver = "1.2.2"
 
 build_exe_options = {
-    "build_exe":"build/v{}win64".format(ver),
+    "build_exe":r"build\v{}win64".format(ver),
     "include_msvcr": True,
     "include_files": [
         path.join(sys.exec_prefix, "VCRUNTIME140.dll") # Потому что include_msvcr=true, по какой-то причине, недостаточно, лол
@@ -19,11 +16,18 @@ build_exe_options = {
     ]
 }
 
+copyfile("frameserver.pyw", "frameserver-cli.py")
+
 setup(
     name = "Advanced FrameServer companion ❤",
     version = ver,
     author = "MahouShoujoMivutilde",
     description = "Advanced FrameServer companion ❤",
     options = {"build_exe": build_exe_options},
-    executables = [Executable("frameserver.pyw", base = base, icon = "server.ico")] # Icon made by Nikita Golubev CC 3.0 BY - http://www.flaticon.com/free-icon/server_362565
+    executables = [
+        Executable("frameserver.pyw", base = "Win32GUI", icon = "server.ico"),
+        Executable("frameserver-cli.py", base = None, icon = "server.ico")
+    ] # Icon made by Nikita Golubev CC 3.0 BY - http://www.flaticon.com/free-icon/server_362565
 )
+
+remove("frameserver-cli.py")
